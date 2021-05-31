6d4d1bfeb9f6f9369c15343794b1d6c8a9c3254e
Takumikimura141899 <kimura141899@gmail.com>
2021-05-31 22:39:40 +0900

    third-commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Room from "./pages/Room";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { NotFound } from "./pages/NotFound";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export const App = () => {
   return (
     <Router>
+      <Link to="/login">ログインへ</Link>
+      <br />
+      <Link to="/signup">サインアップへ</Link>
+      <br />
       <Switch>
         <Route exact path="/" component={Room} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+        <Route component={NotFound} />
       </Switch>
     </Router>
   );
