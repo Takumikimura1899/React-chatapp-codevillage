0d51e43feb6eca86a76cccfdc0ed1464943891c6
Takumikimura141899 <kimura141899@gmail.com>
2021-06-01 22:02:04 +0900

    fourth-commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import React from "react";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Room from "./pages/Room";
 import { NotFound } from "./pages/NotFound";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { AuthProvider } from "./AuthService";
+import LoggedInRoute from "./LoggedInRoute";
 
 export const App = () => {
   return (
-    <Router>
-      <Link to="/login">ログインへ</Link>
-      <br />
-      <Link to="/signup">サインアップへ</Link>
-      <br />
-      <Switch>
-        <Route exact path="/" component={Room} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Link to="/">ルームへ</Link>
+        <br />
+        <Link to="/login">ログインへ</Link>
+        <br />
+        <Link to="/signup">サインアップへ</Link>
+        <br />
+        <Switch>
+          <LoggedInRoute exact path="/" component={Room} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import firebase from "../config/firebase";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const onChangeEmail = (e) => setEmail(e.target.value);
+
+  const [password, setPassword] = useState("");
+  const onChangePassword = (e) => setPassword(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">E-mail</label>
-          <input type="email" id="email" name="email" placeholder="email" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="email"
+            onChange={onChangeEmail}
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
@@ -16,6 +39,7 @@ const Login = () => {
             id="password"
             name="password"
             placeholder="password"
+            onChange={onChangePassword}
           />
         </div>
         <button type="submit">Login</button>
